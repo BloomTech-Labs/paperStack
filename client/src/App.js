038bfc3eed3abac08c18b9e2e475b808d5fd17b0
Dan Volosnikov <danvolosnikov@gmail.com>
2018-03-29 17:44:21 -0400

    accepted head changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Route, Switch } from 'react-router-dom';
 
@@ -8,16 +7,10 @@ import Authorisation from './components/Authorisation';
 import RequireAuth from './components/RequireAuthHOC';
 import InvoiceList from './components/InvoiceList';
 import NotFound from './components/NotFound';
-=======
-import LandingPage from './components/LandingPage';
-import CreateAccount from './components/CreateAccount';
-import './App.css';
->>>>>>> 7ed120e862bafcd4593f71684a6491e56b596720
 
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
       <Router>
         <Switch>
           <Route exact path="/" component={LandingPage} />
@@ -26,12 +19,6 @@ class App extends Component {
           <Route component={NotFound} />
         </Switch>
       </Router>
-=======
-      <div className="App">
-        {/* <LandingPage /> */}
-        <CreateAccount />
-      </div>
->>>>>>> 7ed120e862bafcd4593f71684a6491e56b596720
     );
   }
 }
