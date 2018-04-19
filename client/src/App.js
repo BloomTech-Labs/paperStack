98cd0837778b3654bdcc366226909c199ddfeb30
Jason Nuhn <jason.a.nuhn@gmail.com>
2018-04-18 21:57:56 -0400

    updated app file

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { HashRouter } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
-import LandingPage from './components/LandingPage';
-import Authorisation from './components/Authorisation';
-import RequireAuth from './components/RequireAuthHOC';
-import InvoiceList from './components/InvoiceList';
-import Billing from './components/Billing';
-import Settings from './components/Settings';
-import NotFound from './components/NotFound';
-import InvoiceScreen from './components/invoiceScreen';
-
+import LandingPage from "./components/LandingPage";
+import Authorisation from "./components/Authorisation";
+import RequireAuth from "./components/RequireAuthHOC";
+import InvoiceList from "./components/InvoiceList";
+import Billing from "./components/Billing";
+import Settings from "./components/Settings";
+import NotFound from "./components/NotFound";
+import InvoiceScreen from "./components/invoiceScreen";
 
 class App extends Component {
   render() {
     return (
-      <Router>
+      <HashRouter>
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route path="/authorisation" component={Authorisation} />
           <Route path="/invoices" component={RequireAuth(InvoiceList)} />
-          <Route path="/new" component={(InvoiceScreen)} />
+          <Route path="/new" component={InvoiceScreen} />
           <Route path="/billing" component={Billing} />
           <Route path="/settings" component={RequireAuth(Settings)} />
           <Route component={NotFound} />
         </Switch>
-      </Router>
+      </HashRouter>
     );
   }
 }
